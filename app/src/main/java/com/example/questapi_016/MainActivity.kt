f3aceecd4e06df4851512ddd9b99ae4c00a78f5a
HafizZada16 <hafizzada16@gmail.com>
2025-12-19 20:30:25 +0700

    Mengubah MainActivity.kt

--- app/src/main/java/com/example/questapi_016/MainActivity.kt
+++ app/src/main/java/com/example/questapi_016/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.questapi_016.ui.theme.QuestAPI_016Theme
+import com.example.questapi_016.uicontroller.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +21,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestAPI_016Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestAPI_016Theme {
-        Greeting("Android")
     }
 }
