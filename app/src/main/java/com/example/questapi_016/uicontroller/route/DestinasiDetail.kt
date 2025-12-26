eb48c7d7245415acc6dce8fc96e5df167d458be7
HafizZada16 <hafizzada16@gmail.com>
2025-12-26 22:35:25 +0700

    Fix DestinasiDetail.kt

--- app/src/main/java/com/example/questapi_016/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/questapi_016/uicontroller/route/DestinasiDetail.kt
@@ -4,5 +4,7 @@ import com.example.questapi_016.R
 
 object DestinasiDetail : DestinasiNavigasi {
     override val route = "item_detail"
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
     override val titleRes = R.string.detail_siswa
 }
