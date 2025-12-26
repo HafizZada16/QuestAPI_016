8551b4659d360f5f8f4fe576e258bead4e6668b1
HafizZada16 <hafizzada16@gmail.com>
2025-12-26 21:39:22 +0700

    Menambahkan DestinasiEdit.kt

---
+++ app/src/main/java/com/example/questapi_016/uicontroller/route/DestinasiEdit.kt
@@ -0,0 +1,8 @@
+package com.example.questapi_016.uicontroller.route
+
+import com.example.questapi_016.R
+
+object DestinasiEdit : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+}
