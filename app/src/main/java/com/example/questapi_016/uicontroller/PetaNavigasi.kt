dcfd6bb09b96fc2d8b45ab672d8d4c1608eb7bf5
HafizZada16 <hafizzada16@gmail.com>
2025-12-26 23:46:36 +0700

    Fix PetaNavigasi.kt

--- app/src/main/java/com/example/questapi_016/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/questapi_016/uicontroller/PetaNavigasi.kt
@@ -3,12 +3,17 @@ package com.example.questapi_016.uicontroller
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.questapi_016.uicontroller.route.DestinasiDetail
+import com.example.questapi_016.uicontroller.route.DestinasiEdit
 import com.example.questapi_016.uicontroller.route.DestinasiEntry
 import com.example.questapi_016.uicontroller.route.DestinasiHome
+import com.example.questapi_016.view.DetailSiswaScreen
+import com.example.questapi_016.view.EditSiswaScreen
 import com.example.questapi_016.view.EntrySiswaScreen
 import com.example.questapi_016.view.HomeScreen
 
@@ -32,8 +37,21 @@ fun HostNavigasi(
                 })
         }
         composable(DestinasiEntry.route){
-            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome
-                .route) })
+            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) })
+        }
+        composable(DestinasiDetail.routeWithArgs, arguments = listOf(navArgument(DestinasiDetail.itemIdArg) {
+            type = NavType.IntType })
+        ){
+            DetailSiswaScreen(navigateToEditItem = {navController.navigate("${DestinasiEdit.route}/$it")},
+                navigateBack = { navController.navigate(DestinasiHome.route) })
+        }
+        composable(DestinasiEdit.routeWithArgs, arguments = listOf(navArgument(DestinasiEdit.itemIdArg) {
+            type = NavType.IntType })){
+            EditSiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) },
+                onNavigateUp = { navController.navigateUp() })
         }
     }
 }
+
+
+
--- app/src/main/java/com/example/questapi_016/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/questapi_016/uicontroller/route/DestinasiDetail.kt
@@ -3,7 +3,7 @@ package com.example.questapi_016.uicontroller.route
 import com.example.questapi_016.R
 
 object DestinasiDetail : DestinasiNavigasi {
-    override val route = "item_detail"
+    override val route = "detail_siswa"
     const val itemIdArg = "idSiswa"
     val routeWithArgs = "$route/{$itemIdArg}"
     override val titleRes = R.string.detail_siswa
