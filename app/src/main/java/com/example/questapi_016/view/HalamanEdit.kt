d6c7104e6195615f2f52d2f9ee78070e13f6f654
HafizZada16 <hafizzada16@gmail.com>
2025-12-26 21:54:55 +0700

    Menambahkan Scaffold dan TopAppBar untuk Edit Screen

--- app/src/main/java/com/example/questapi_016/view/HalamanEdit.kt
+++ app/src/main/java/com/example/questapi_016/view/HalamanEdit.kt
@@ -1,11 +1,17 @@
 package com.example.questapi_016.view
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questapi_016.uicontroller.route.DestinasiEdit
 import com.example.questapi_016.viewmodel.EditViewModel
 import com.example.questapi_016.viewmodel.provider.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -15,5 +21,16 @@ fun EditSiswaScreen(
     modifier: Modifier = Modifier,
     viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
+    Scaffold(
+      topBar = {
+          SiswaTopAppBar(
+              title = stringResource(DestinasiEdit.titleRes),
+              canNavigateBack = true,
+              navigateUp = onNavigateUp
+          )
+      },
+        modifier = modifier
+    ){ innerPadding ->
 
+    }
 }
