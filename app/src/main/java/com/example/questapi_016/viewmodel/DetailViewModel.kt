7e079014304acbe4d6440de313670a4fdc4d83d6
HafizZada16 <hafizzada16@gmail.com>
2025-12-26 22:36:09 +0700

    Fix DetailViewModel.kt

--- app/src/main/java/com/example/questapi_016/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_016/viewmodel/DetailViewModel.kt
@@ -12,18 +12,20 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.serialization.InternalSerializationApi
 import com.example.questapi_016.modeldata.DataSiswa
 import com.example.questapi_016.repositori.RepositoryDataSiswa
+import com.example.questapi_016.uicontroller.route.DestinasiDetail
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
 import retrofit2.Response
 import java.io.IOException
 
+
 sealed interface StatusUIDetail {
-    data class Success(val SatuSiswa: DataSiswa) : StatusUIDetail
+    data class Success(val satusiswa: DataSiswa) : StatusUIDetail
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
 }
 class DetailViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel() {
-    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.idSiswaArg])
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
     var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
         private set
 
