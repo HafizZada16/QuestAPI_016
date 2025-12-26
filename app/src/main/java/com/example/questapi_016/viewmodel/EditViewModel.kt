a981378c82b20ab144a857f7145f55e6b23bef60
HafizZada16 <hafizzada16@gmail.com>
2025-12-26 21:02:50 +0700

    Menambahkan Fungsi Load data siswa tunggal saat inisialisasi ViewModel

--- app/src/main/java/com/example/questapi_016/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_016/viewmodel/EditViewModel.kt
@@ -5,13 +5,22 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.questapi_016.modeldata.UIStateSiswa
+import com.example.questapi_016.modeldata.toUiStateSiswa
 import com.example.questapi_016.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
 
 class EditViewModel(savedStateHandle: SavedStateHandle,private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
 
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.idSiswaArg])
 
-
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
 }
