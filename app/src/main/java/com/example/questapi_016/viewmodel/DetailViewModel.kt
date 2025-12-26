4a8671bdd4424b18577c125b36264004c6c2e064
HafizZada16 <hafizzada16@gmail.com>
2025-12-26 21:21:55 +0700

    Menambahkan Inisialisasi ViewModel Detail Siswa

--- app/src/main/java/com/example/questapi_016/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_016/viewmodel/DetailViewModel.kt
@@ -2,10 +2,27 @@
 
 package com.example.questapi_016.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import kotlinx.serialization.InternalSerializationApi
 import com.example.questapi_016.modeldata.DataSiswa
+import com.example.questapi_016.repositori.RepositoryDataSiswa
 
 sealed interface StatusUIDetail {
     data class Success(val SatuSiswa: DataSiswa) : StatusUIDetail
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
+}
+class DetailViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel() {
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.idSiswaArg])
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
+    init {
+        getDataSiswa()
+    }
+
 }
